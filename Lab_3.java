6eb9ae2c07c664151f7ad5e5a794a62cac4e3ff3
Alexchernoshtan <48185881+Alexchernoshtan@users.noreply.github.com>
2019-03-04 07:47:04 +0200

    Update Lab_3.java

--- Lab_3.java
+++ Lab_3.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Lab_3 {
     void lab3() {
         String str1="hehho wwos ffas";
@@ -7,14 +5,18 @@ public class Lab_3 {
         str1.trim();
         String [] str2=str1.split(" ");
 
-
         for(int i =0; i<str2.length;i++){
             for(int j=1;j<str2.length;j++){
-                if(str2[0]==str2[j]){
-                    System.out.println(str2[i]=str2[i].substring(0, j) + str1.substring(j+1));
+                if(str2[i].charAt(0)==str2[i].charAt(j)){
+                    //str2[i]=str2[i].substring(0,j) + str2[i].substring(j+1);
+                    char l=str2[i].charAt(0);
+                    str2[i]=str2[i].replace(l,'//s');
                 }
             }
+
+            System.out.println(str2[i]);
         }
+
 
 
 
