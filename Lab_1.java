d38115916a44798a2f48a01b2fd4608e1c1650b7
Alexchernoshtan <48185881+Alexchernoshtan@users.noreply.github.com>
2019-03-10 18:28:04 +0200

    Update Lab_1.java

--- Lab_1.java
+++ Lab_1.java
@@ -1,50 +1,45 @@
-import java.util.Scanner;
-public class Main {
-
-    public static void main(String[] args) {
-
-            try {
-
-                Scanner in = new Scanner(System.in);
-                System.out.println("Input lab number: ");
-                int a = in.nextInt();
-                switch (a) {
-                    case 1:
-                        System.out.println("Input double a1: ");
-                        double a1 = in.nextDouble();
-                        System.out.println("Input double b: ");
-                        double b = in.nextDouble();
-                        System.out.println("Input double n: ");
-                        double n = in.nextDouble();
-                        System.out.println("Input double m: ");
-                        double m = in.nextDouble();
-                        System.out.println("Input int con: ");
-                        int con = in.nextInt();
-
-                        Lab_1 one = new Lab_1();
-                        one.setA1(a1);
-                        one.setB(b);
-                        one.setCon(con);
-                        one.setM(m);
-                        one.setN(n);
-                        one.lab1();
-                        break;
-                    case 2:
-                        Lab_2 two = new Lab_2();
-                        two.lab2();
-                        break;
-                    case 3:
-                        Lab_3 three = new Lab_3();
-                        three.lab3();
-                        break;
-                    default:
-                        System.out.println("Этой лабораторной нету,введите другой номер");
-                }
-
-            } catch (java.util.InputMismatchException ex) {
-                System.out.println("Введите число!!");
-            }
+public class Lab_1 {
+    private double a1;
+    private double b;
+    private double n;
+    private double m;
+    private int con;
+    private double s = 0;
+    public void setA1(double a1){
+        this.a1=a1;
     }
 
-}
+    public  void setB(double b){
+        this.b=b;
+    }
 
+    public  void setN(double n){
+        this.n=n;
+    }
+
+    public  void setM(double m){
+        this.m=m;
+    }
+
+    public  void setCon(int con){
+        this.con=con;
+    }
+
+    void lab1() {
+        try {
+            try {
+                for (double i = a1; i <= n; i++) {
+                    for (double j = b; j <= m; j++) {
+                        s += (i % j) / (i - con);
+                    }
+                }
+            }catch(ArithmeticException ex){
+                System.out.println("i-con=0!Исправьте это");
+            }
+            System.out.println("s=" + s);
+        }catch(java.util.InputMismatchException ex){
+            System.out.println("Введите правильный тип переменных!!!");
+        }
+
+    }
+}
