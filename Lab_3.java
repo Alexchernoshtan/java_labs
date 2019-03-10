71325add520edc34c29e9b4408fdf490b2f5e252
Alexchernoshtan <48185881+Alexchernoshtan@users.noreply.github.com>
2019-03-10 18:28:29 +0200

    Update Lab_3.java

--- Lab_3.java
+++ Lab_3.java
@@ -1,27 +1,19 @@
 public class Lab_3 {
+
     void lab3() {
-        String str1="hehho wwos ffas";
-        System.out.println(str1);
-        str1.trim();
-        String [] str2=str1.split(" ");
-
-        for(int i =0; i<str2.length;i++){
-            for(int j=1;j<str2.length;j++){
-                if(str2[i].charAt(0)==str2[i].charAt(j)){
-                    //str2[i]=str2[i].substring(0,j) + str2[i].substring(j+1);
-                    char l=str2[i].charAt(0);
-                    str2[i]=str2[i].replace(l,'//s');
-                }
+        String text="hehho wwos ffas";
+        String[] words= text.split(" ");
+        text=" ";
+        for(int i=0; i<words.length;i++){
+            if(words[i].isEmpty()){
+                continue;
             }
-
-            System.out.println(str2[i]);
+            String first = String.valueOf(words[i].charAt(0));
+            words[i] = words[i].replaceAll(first,"");
+            first +=words[i];
+            text += first+ " ";
         }
-
-
-
-
-
-
-
+        System.out.println(text);
     }
 }
+
